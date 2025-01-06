8b4dc18564fd0c7d4f617eed51cac20b3565ba61
Moses <mosa.khaldun@miswag.com>
2025-01-06 11:43:41 +0300

    Initial commit

---
+++ MGTooltips/MGTooltips.h
@@ -0,0 +1,18 @@
+//
+//  MGTooltips.h
+//  MGTooltips
+//
+//  Created by Miswag on 06/01/2025.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MGTooltips.
+FOUNDATION_EXPORT double MGTooltipsVersionNumber;
+
+//! Project version string for MGTooltips.
+FOUNDATION_EXPORT const unsigned char MGTooltipsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MGTooltips/PublicHeader.h>
+
+
